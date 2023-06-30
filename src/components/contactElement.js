2f89fc743fa29f2f989d031fa4758efa76d52770
Gustavo Morales <gusanmore@gmail.com>
2023-06-30 10:14:00 -0600

    conecting email to contact

--- src/components/contactElement.js
+++ src/components/contactElement.js
@@ -13,7 +13,10 @@ export class ContactELement extends LitElement {
     render() {
         return html`
         
-            <form class="bg-blue-300 flex flex-col mx-auto justify-center p-5 h-screen" action="#">
+            <form class="bg-blue-300 flex flex-col mx-auto justify-center p-5 h-screen" 
+                action="https://formspree.io/f/mleyranl"
+                method="POST"
+            >
     
                 <div class="container w-11/12 mx-auto">
                     <h2 class="uppercase text-center text-2xl font-bolder">
@@ -22,15 +25,15 @@ export class ContactELement extends LitElement {
                     </h2>
                     
                     <div class="flex flex-col sm:flex-row w-full justify-between m-2 p-2">
-                        <input class="rounded-xl ring focus:ring-blue-400 outline-none m-3 p-2 sm:w-1/2" type="text" name="" id="" placeholder="Name">
-                        <input class="rounded-xl ring focus:ring-blue-400 outline-none m-3 p-2 sm:w-1/2" type="text" name="" id="" placeholder="Subjet">
+                        <input class="rounded-xl ring focus:ring-blue-400 outline-none m-3 p-2 sm:w-1/2" type="text" name="email" id="" placeholder="Email">
+                        <input class="rounded-xl ring focus:ring-blue-400 outline-none m-3 p-2 sm:w-1/2" type="text" name="subjet" id="" placeholder="Subjet">
                     </div>
                     
                     <div class="flex justify-center w-full m-2 p-2">
-                        <textarea class="rounded-xl w-full max-h-96 h-40 min-h-40 ring focus:ring-blue-400 outline-none p-2 mx-3" name="" id="" rows="auto" placeholder="Write your message"></textarea>
+                        <textarea class="rounded-xl w-full max-h-96 h-40 min-h-40 ring focus:ring-blue-400 outline-none p-2 mx-3" name="message" id="" rows="auto" placeholder="Write your message"></textarea>
                     </div>
                     
-                    <button class="bg-blue-500 text-white rounded-xl px-10 capitalize py-2 mx-7">enviar</button>
+                    <button type="submit" class="bg-blue-500 text-white rounded-xl px-10 capitalize py-2 mx-7">enviar</button>
                 </div>
         
             </form>`;
